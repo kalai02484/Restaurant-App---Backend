c2e9962f5dfde6e04e071a318aefe6843385f086
kalai02484 <kalai02484@gmail.com>
2026-09-03 05:13:27 +0530

    Base Code

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./Database/dbConfig.js";
+
+dotenv.config();
+
+//app init
+const app = express();
+
+//default mddlewares
+app.use(cors());
+app.use(express.json());
+
+//DB connect 
+connectDB();
+
+//default Route
+app.get("/", (req, res)=>{
+    res.status(200).send("Welcome to backend");
+})
+
+//Custom Routes
+
+//Port
+const port = process.env.PORT || 5000;
+
+//Starting server
+app.listen(port, ()=>{
+    console.log(`Server Started on the port ${port}`);
+})
