8182a21516926e7f3e88a100e3fe530c7314cc3a
kalai02484 <kalai02484@gmail.com>
2026-09-04 18:28:33 +0530

    authMiddleware and get current user Added

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -152,3 +152,13 @@ export const loginUser = async (req, res) => {
     });
   }
 };
+
+//Get Current User
+export const getCurrentUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select("-password");
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- Routers/authRoute.js
+++ Routers/authRoute.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { registerUser, loginUser } from "../Controllers/authController.js";
+import {
+  registerUser,
+  loginUser,
+  getCurrentUser,
+} from "../Controllers/authController.js";
+import { authMiddleware } from "../Middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/getuser", authMiddleware, getCurrentUser);
 
 export default router;
